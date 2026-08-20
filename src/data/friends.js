f821c1f2dbd4dac4acb759ccfd541733f2ecd252
ZHOENG <ZHOENGGROUP@163.com>
2026-08-20 22:37:54 +0800

    Create friends.js
    Add friendGroups data structure in friends.js.

---
+++ src/data/friends.js
@@ -0,0 +1,40 @@
+// src/data/friends.js
+const friendGroups = [
+  {
+    title: '河北正定中学',
+    description: '河北正定中学官方网站',
+    friends: [
+      {
+        title: '校本部',
+        description: '河北正定中学官网 - 校本部',
+        href: 'http://www.zhengzhong.cn/home.html',
+        avatar: 'https://zdzx.zhoeng.com.cn/img/zdzx.png',
+      },
+      {
+        title: '东校区',
+        description: '河北正定中学官网 - 东校区',
+        href: 'http://dongxiaoqu.zhengzhong.cn/',
+        avatar: 'https://zdzx.zhoeng.com.cn/img/zdzx.png',
+      },
+      {
+        title: '国际部',
+        description: '河北正定中学官网 - 国际部',
+        href: 'http://dongxiaoqu.zhengzhong.cn/intro/29.html',
+        avatar: 'https://zdzx.zhoeng.com.cn/img/zdzx.png',
+      },
+    ],
+  },
+  {
+    title: '中政集团',
+    description: '中政集团官方网站',
+    friends: [
+      {
+        title: '中政集团',
+        description: '中政集团官网',
+        href: 'https://www.zhoeng.com.cn/',
+      },
+    ],
+  },
+];
+
+export default friendGroups;
