c78800ebd7d5768c96cfab3cb13db54ceaaf17e0
ZHOENG <ZHOENGGROUP@163.com>
2026-08-20 09:15:31 +0800

    Create changelog.js
    Add changelog.js with initial changelog entries.

---
+++ src/data/changelog.js
@@ -0,0 +1,54 @@
+// src/data/changelog.js
+export const TYPE_LABEL = {
+  added: '新增',
+  changed: '变更',
+  removed: '移除',
+  improved: '改进',
+  upgraded: '升级',
+  fixed: '修复',
+  deprecated: '弃用',
+  released: '发布',
+};
+
+export const CHANGELOG_LIST = [
+  {
+    date: '2026-08-19',
+    type: 'released',
+    content: '网站 <a href="https://zdzx.zhoeng.com.cn/">河北正定中学 · 线上活动中心</a> 发布首页 <code>v1.0.0</code>',
+  },
+  {
+    date: '2026-08-18',
+    type: 'added',
+    content: '申请为 <code>zdzx.zhoeng.com.cn</code> 域名接入 <code>DocSearch</code> 文档搜索功能',
+  },
+  {
+    date: '2026-08-18',
+    type: 'added',
+    content: '注册 <code>DocSearch</code> 账号 <code>zhoenggroup@163.com</code>',
+  },
+  {
+    date: '2026-08-18',
+    type: 'added',
+    content: '建立网站 <a href="https://zdzx.zhoeng.com.cn/">河北正定中学 · 线上活动中心</a>，使用 <a href="https://docusaurus.io/changelog/3.10.2">Docusaurus v3.10.2</a> 构建，支持 <b>KaTeX</b> 数学公式，部署在 <a href="https://pages.github.com">GitHub Pages</a>',
+  },
+  {
+    date: '2026-08-18',
+    type: 'added',
+    content: '创建 <code>Github</code> 仓库 <code>github.com/ZHOENGGROUP/zdzx</code>',
+  },
+  {
+    date: '2026-08-18',
+    type: 'added',
+    content: '解析域名 <code>zdzx.zhoeng.com.cn</code>',
+  },
+  {
+    date: '2026-07-26',
+    type: 'added',
+    content: '注册 <code>Github</code> 账号 <code>github.com/ZHOENGGROUP</code>',
+  },
+  {
+    date: '2026-07-25',
+    type: 'added',
+    content: '注册域名 <code>zhoeng.com.cn</code>',
+  },
+];
