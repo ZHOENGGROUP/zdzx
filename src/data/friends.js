343695c40df00d11aa3b346d4bc41f7622ce749d
ZHOENG <ZHOENGGROUP@163.com>
2026-08-21 20:48:26 +0800

    Update friends.js
    Add avatarShape property to friend group objects.

--- src/data/friends.js
+++ src/data/friends.js
@@ -9,18 +9,21 @@ const friendGroups = [
         description: '河北正定中学官网 - 校本部',
         href: 'http://www.zhengzhong.cn/home.html',
         avatar: 'https://zdzx.zhoeng.com.cn/img/zdzx.png',
+        avatarShape: 'original',      // 默认圆形，可改为 'square' 或 'original'
       },
       {
         title: '东校区',
         description: '河北正定中学官网 - 东校区',
         href: 'http://dongxiaoqu.zhengzhong.cn/',
         avatar: 'https://zdzx.zhoeng.com.cn/img/zdzx.png',
+        avatarShape: 'original',
       },
       {
         title: '国际部',
         description: '河北正定中学官网 - 国际部',
         href: 'http://dongxiaoqu.zhengzhong.cn/intro/29.html',
         avatar: 'https://zdzx.zhoeng.com.cn/img/zdzx.png',
+        avatarShape: 'original',
       },
     ],
   },
@@ -32,6 +35,8 @@ const friendGroups = [
         title: '中政集团',
         description: '中政集团官网',
         href: 'https://www.zhoeng.com.cn/',
+        avatar: '',
+        avatarShape: 'circle',       // 无头像时显示默认占位图标
       },
     ],
   },
