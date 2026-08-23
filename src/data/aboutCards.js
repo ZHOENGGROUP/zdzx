fa4ec3a99c63e523748ed1d446a902ed513ad88d
ZHOENG <ZHOENGGROUP@163.com>
2026-08-23 15:00:25 +0800

    Update aboutCards.js
    Update links and descriptions in aboutCards.js.

--- src/data/aboutCards.js
+++ src/data/aboutCards.js
@@ -2,33 +2,33 @@ const aboutCards = [
   {
     title: '河北正定中学',
     description: '河北正定中学官方网站',
-    link: 'http://www.zhengzhong.cn/home.html',
+    link: 'http://www.zhengzhong.cn/',
     type: 'image',
     src: '/img/zdzx.png',
-    shape: 'circle',
+    shape: 'original',
   },
   {
     title: '中政集团',
     description: '中政集团官网',
     link: 'https://www.zhoeng.com.cn/',
     type: 'icon',
-    icon: 'lucide:building-2',              // Lucide 图标：大楼
-    shape: 'circle',                         // 形状统一为圆形，与图片一致
+    icon: 'lucide:building-2',
+    shape: 'circle',
   },
   {
     title: 'GitHub',
-    description: '代码托管与开源社区',
+    description: '正中中心开源项目仓库',
     link: 'https://github.com/ZHOENGGROUP/zdzx',
     type: 'icon',
-    icon: 'lucide:github',                   // Lucide 图标：GitHub
+    icon: 'lucide:github',
     shape: 'circle',
   },
   {
     title: '帮助中心',
-    description: '使用文档与常见问题',
+    description: '帮助中心页面',
     link: '/docs/help',
     type: 'icon',
-    icon: 'lucide:book-open',                // Lucide 图标：打开的书
+    icon: 'lucide:book-open',
     shape: 'circle',
   },
 ];
