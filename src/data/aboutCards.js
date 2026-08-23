7979a241280f794aaf315c3cc1b02b1f2fc24140
ZHOENG <ZHOENGGROUP@163.com>
2026-08-23 14:21:19 +0800

    Update aboutCards.js
    Update icon types and sources in aboutCards.js.

--- src/data/aboutCards.js
+++ src/data/aboutCards.js
@@ -3,17 +3,16 @@ const aboutCards = [
     title: '河北正定中学',
     description: '河北正定中学官方网站',
     link: 'http://www.zhengzhong.cn/home.html',
-    type: 'image',                    // 'icon' 或 'image'
-    icon: 'lucide:school',            // 当 type='icon' 时使用
-    src: '/img/zdzx.png',             // 当 type='image' 时使用
-    shape: 'circle',                  // 'circle' | 'square' | 'rounded-square' | 'original'
+    type: 'image',
+    src: '/img/zdzx.png',
+    shape: 'circle',
   },
   {
     title: '中政集团',
     description: '中政集团官网',
     link: 'https://www.zhoeng.com.cn/',
     type: 'icon',
-    icon: 'lucide:building-2',        // 黑色图标
+    icon: 'mdi:office-building',            // 更换为 mdi 图标
     shape: 'rounded-square',
   },
   {
@@ -21,7 +20,7 @@ const aboutCards = [
     description: '代码托管与开源社区',
     link: 'https://github.com/ZHOENGGROUP/zdzx',
     type: 'icon',
-    icon: 'lucide:github',
+    icon: 'mdi:github',                      // 与顶部栏风格统一
     shape: 'original',
   },
   {
@@ -29,7 +28,7 @@ const aboutCards = [
     description: '使用文档与常见问题',
     link: '/docs/help',
     type: 'icon',
-    icon: 'lucide:book-open',
+    icon: 'mdi:help-circle',                 // 更换为 mdi 图标
     shape: 'square',
   },
 ];
